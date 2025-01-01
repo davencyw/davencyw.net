1b8a0ec94b456fc19fde33a8f70940df3ce69289
david <davidschmidig@secondspectrum.com>
2025-01-01 21:04:27 +0100

    config files

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+
+export default defineConfig({
+    root: 'public', // Serve files from the public directory
+    base: '/davencyw/davencyw.net/', // Replace with your repository name
+  })
