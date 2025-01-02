10dda9d136a4fa334555ea0bf8f2f19d239af7dd
david <davidschmidig@secondspectrum.com>
2025-01-02 08:44:20 +0100

    structure update

---
+++ page/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+
+  export default defineConfig({
+    base: '/davencyw.net/', // Set this to match your repository name
+    assetsInclude: ['**/projects/**/*.html'], // Include all .html files under projects/
+  })
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-
-export default defineConfig({
-    root: 'public', // Serve files from the public directory
-    base: '/davencyw/davencyw.net/', // Replace with your repository name
-  })
