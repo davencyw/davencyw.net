e1bf8b3bb54381e9ca6574810d3eda39f9b73665
david <davidschmidig@secondspectrum.com>
2025-01-07 08:26:49 +0100

    updates

--- page/vite.config.js
+++ page/vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 
   export default defineConfig({
     base: '/davencyw.net/', // Set this to match your repository name
-    assetsInclude: ['**/projects/**/*.html'], // Include all .html files under projects/
+    assetsInclude: ['**/projects/**/*.html', '**/projects/**/*.png'], // Include all necessary files under projects/
   })
